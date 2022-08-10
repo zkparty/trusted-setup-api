ca27c5f660e699672fe0dbdc42417b092860db60
Nico Serrano <nnicosp@hotmail.com>
2022-08-10 15:17:09 -0400

    api-server(fix): change to api instead of app

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import {config as dotEnvConfig} from 'dotenv';
+import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+import { router as ceremonyRoutes } from './routes/ceremony';
+import serviceAccount from './firebase_skey.json';
+
+dotEnvConfig();
+
+initializeApp({ credential: cert(serviceAccount as ServiceAccount) });
+
+const app = express();
+const port = process.env.PORT;
+
+app.use('/ceremony', ceremonyRoutes);
+
+app.listen(port, async () => {
+  console.log(`⚡️[server]: Running at https://localhost:${port}`);
+});
