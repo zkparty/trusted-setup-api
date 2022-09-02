a06332aa2dd39c3e970637160f03f0b9750453b2
Nico Serrano <nnicosp@hotmail.com>
2022-09-01 21:13:44 -0500

    env(fix): better env naming

--- src/controllers/ceremony.ts
+++ src/controllers/ceremony.ts
@@ -6,7 +6,7 @@ import { Ceremony } from '../models/ceremony';
 
 dotEnvConfig();
 const DOMAIN: string = process.env.DOMAIN!;
-const AVERAGE_TIME_IN_SECONDS = Number(process.env.AVERAGE_TIME_IN_SECONDS!);
+const SECONDS_AVERAGE_COMPUTE_TIME = Number(process.env.SECONDS_AVERAGE_COMPUTE_TIME!);
 
 export async function createCeremony(ceremony: Ceremony): Promise<WriteResult> {
     const db = getFirestore();
@@ -20,7 +20,7 @@ export async function createCeremony(ceremony: Ceremony): Promise<WriteResult> {
     ceremony.currentIndex = 0;
     ceremony.lastValidIndex = 0;
     ceremony.highestQueueIndex = -1;
-    ceremony.averageSecondsPerContribution = AVERAGE_TIME_IN_SECONDS;
+    ceremony.averageSecondsPerContribution = SECONDS_AVERAGE_COMPUTE_TIME;
     const result = await db.collection('ceremonies').doc(DOMAIN).set(ceremony);
     return result;
 }
