aaf2e01ccfca87efdf2e834e80c44e0c6e7ddc09
Nico Serrano <nnicosp@hotmail.com>
2022-09-01 20:58:38 -0500

    docs(fix): delete extra comments

--- src/routes/ceremony.ts
+++ src/routes/ceremony.ts
@@ -5,43 +5,6 @@ import { Ceremony } from '../models/ceremony';
 
 const router = express.Router();
 
-/**
- * @api {post} /ceremony/create Create a new ceremony
- * @apiName createCeremony
- * @apiGroup Ceremony
- * @apiDescription This method can be called only once by the ceremony coordinator.
- * In case another ceremony needs to be performed, a new API server should be deployed.
- * @apiBody {String} title="A Test Ceremony"
- * @apiBody {String} description="This is a test for API development"
- * @apiBody {String} circuitFileName="fileNameGoesHere"
- * @apiBody {String} mode="POWERSOFTAU"
- * @apiBody {String} instructions="Enter the text content instructions to show to participants"
- * @apiBody {String} github="https://github.com/zkparty/setup-mpc-ui"
- * @apiBody {String} homepage="https://trustedsetuptest.web.app"
- * @apiBody {String} adminAddr="admin@example.com"
- * @apiBody {Number} startTime=1661374869
- * @apiBody {Number} endTime=1662374869
- * @apiBody {Number} minParticipants="2"
- * @apiSuccess {Object} _writeTime firestore saving result response (use seconds to build a Date object)
- * @apiSuccess {Number} _writeTime._seconds
- * @apiSuccess {Number} _writeTime._nanoseconds
- * @apiSuccessExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  {
- *    "_writeTime": {
- *      "_seconds": 1660170237,
- *      "_nanoseconds": 176469000
- *    }
- *  }
- * @apiError {Number} code
- * @apiError {String} message
- * @apiErrorExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  {
- *    "code": -1,
- *    "message": "Ceremony is already created"
- *  }
- */
 router.post('/create', async (req: Request, res: Response) => {
     const createdCeremony = await getCeremony();
     if (createdCeremony){
@@ -55,25 +18,6 @@ router.post('/create', async (req: Request, res: Response) => {
     }
 });
 
-/**
- * @api {get} /ceremony/status Retrieve status of ceremony
- * @apiName statusCeremony
- * @apiGroup Ceremony
- * @apiDescription Get the status information of the server ceremony
- * @apiSuccess {Object} status firestore result response
- * @apiSuccessExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  {
- *    "status": {
- *      "title": "A Test Ceremony",
- *      "description": "This is a test for API development",
- *      ...
- *    }
- *  }
- *  @apiErrorExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  { }
- */
 router.get('/status', async (_req: Request, res: Response) => {
     const ceremony = await getCeremony();
     res.json(ceremony || {});
--- src/routes/participant.ts
+++ src/routes/participant.ts
@@ -42,77 +42,14 @@ router.use(session({ secret: COOKIE_SESSION_SECRET, resave: false, saveUninitial
 router.use(passport.initialize());
 router.use(ceremonyExists);
 
-/**
- * @api {post} /participant/login/address Log in as a ceremony participant using a public address
- * @apiName loginParticipantWithAddress
- * @apiGroup Participant
- * @apiDescription Every ceremony's participant needs to be logged in to
- * avoid sybil attacks (multiple accounts). The signature contains a shared message
- * and the address attribute is compared to the recovered address of the signature to authenticate
- * @apiBody {String} address="0xfAce669798EbFA92Ec1e47Adc86b1eA213F564bD"
- * @apiBody {String} signature="0x0b89b638bcb4dc234507bcc426ac2324bbb5e3c17ad68545a05c4d39f6e83a4c631a6a30e3af78c8f70c50dc797be24f660e622896e1c5456999507286c938541b"
- * @apiSuccess {String} token
- * @apiSuccess {String} message
- * @apiSuccess {Number} code
- * @apiError {String} message
- * @apiError {Number} code
- * @apiSuccessExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  {
- *    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjB4NUNhNTY1NjlENUNDNTA4MmMzOWEyYTAzMWEwYzYzMTQzMTYzOGI5NyIsImlhdCI6MTUxNjIzOTAyMn0.I8BXaPkl65vjwYpL7Xf3uDBhPusunGMb90pfCE7BxL8",
- *    "message": "Participant logged in",
- *    "code": 0
- *  }
- * @apiErrorExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *   "message": "Address is too new"
- *   "code": -1
- * }
- */
 router.post('/login/address', async (req: Request, res: Response) => {
     const loginRequest = req.body as LoginRequest;
     const result = await loginParticipantWithAddress(loginRequest);
     res.json(result);
 });
 
-/**
- * @api {get} /participant/login/github Log in as a ceremony participant using a Github profile
- * @apiName loginParticipantWithGithub
- * @apiGroup Participant
- * @apiDescription Every ceremony's participant needs to be logged in to
- * avoid sybil attacks (multiple accounts). This route allows participants
- * to login using a Github account. Calling this URL in a browser would redirect
- * to Github for authorization and if approved, it would redirect back to the API callback route
- * @apiSuccess {String} token
- * @apiSuccess {String} message
- * @apiSuccess {Number} code
- * @apiError {String} message
- * @apiError {Number} code
- * @apiSuccessExample {json} Success-Response:
- *  HTTP/1.1 200 OK
- *  {
- *    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjB4NUNhNTY1NjlENUNDNTA4MmMzOWEyYTAzMWEwYzYzMTQzMTYzOGI5NyIsImlhdCI6MTUxNjIzOTAyMn0.I8BXaPkl65vjwYpL7Xf3uDBhPusunGMb90pfCE7BxL8",
- *    "message": "Participant logged in",
- *    "code": 0
- *  }
- * @apiErrorExample {json} Success-Response:
- * HTTP/1.1 200 OK
- * {
- *   "message": "Github profile created after minimum creation time"
- *   "code": -1
- * }
- */
 router.get('/login/github', passport.authenticate('github', { scope: [ 'user:email' ] }), (_req: Request, _res: Response) => {});
 
-/**
- * @api {get} /participant/login/github/callback API callback after Github authorization
- * @apiName GithubCallbackRoute
- * @apiGroup Participant
- * @apiDescription This route is used internally. The user calls the login with Github route,
- * authorizes the request in github.com and then it is redirected to this route for login or user creation.
- * @apiSampleRequest off
- */
 router.get('/login/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), async (req: Request, res: Response) => {
     const githubUser = req.user! as GithubUserProfile;
     const result = await loginParticipantWithGithub(githubUser);
